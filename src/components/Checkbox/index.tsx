73878353f83ad88af2f268de13022bc40369156f
HyeBinYang <skdisk7368@gmail.com>
2025-03-23 14:06:41 +0900

    feat: Checkbox 컴포넌트 추가

---
+++ src/components/Checkbox/index.tsx
@@ -0,0 +1,20 @@
+import { InputHTMLAttributes } from "react";
+import style from "./Checkbox.module.css";
+
+type Props = {
+  htmlFor: string;
+  label?: string;
+  checked?: boolean;
+  onChange?: () => void;
+} & InputHTMLAttributes<HTMLInputElement>;
+
+const Checkbox = ({ htmlFor, label, checked, onChange, ...props }: Props) => {
+  return (
+    <label htmlFor={htmlFor}>
+      <input className={style["checkbox-input"]} id={htmlFor} type="checkbox" checked={checked} onChange={onChange} {...props} />
+      <span className={style["checkbox-label"]}>{label}</span>
+    </label>
+  );
+};
+
+export default Checkbox;
