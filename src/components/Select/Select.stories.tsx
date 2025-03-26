36605ab97c99c5850d50a7de41d29e3a9178bb94
HyeBinYang <skdisk7368@gmail.com>
2025-03-26 23:05:10 +0900

    fix: label 길이에 대한 너비 적용 안되는 버그 수정

--- src/components/Select/Select.stories.tsx
+++ src/components/Select/Select.stories.tsx
@@ -22,6 +22,7 @@ type Story = StoryObj<typeof meta>;
 
 export const Test: Story = {
   args: {
-    label: "Age",
+    label: "qweqweqweqweqweqw",
+    width: 200,
   },
 };
--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import style from "./Select.module.css";
 import classNames from "classnames";
 import Flex from "../Flex";
@@ -11,9 +11,12 @@ type SelectOption = {
 type Props = {
   options: SelectOption[];
   label: string;
+  width?: number;
 };
 
-const Select = ({ label }: Props) => {
+const Select = ({ label, width = 120 }: Props) => {
+  const labelWrapperRef = useRef<HTMLDivElement>(null);
+  const labelRef = useRef<HTMLLabelElement>(null);
   const [open, setOpen] = useState(false);
 
   const controlClass = classNames({
@@ -37,21 +40,55 @@ const Select = ({ label }: Props) => {
     [style["select-control-right"]]: true,
   });
 
+  const handleClick = () => {
+    setOpen(true);
+  };
+
+  const handleBlur = () => {
+    setOpen(false);
+  };
+
+  useEffect(() => {
+    if (labelWrapperRef.current && labelRef.current) {
+      labelWrapperRef.current.style.width = open ? `${labelRef.current.clientWidth * 0.7}px` : "";
+    }
+  }, [open]);
+
   return (
-    <div className={controlClass} tabIndex={0} onClick={() => setOpen(true)} onBlur={() => setOpen(false)}>
+    <div
+      className={controlClass}
+      tabIndex={0}
+      style={{
+        width,
+      }}
+      onClick={handleClick}
+      onBlur={handleBlur}
+    >
       <Flex>
         <div className={controlLeftClass} />
-        <div className={labelWrapperClass}>{label && <label className={labelClass}>{label}</label>}</div>
+        <div
+          className={labelWrapperClass}
+          ref={labelWrapperRef}
+          style={{
+            maxWidth: open ? "none" : `${width - 44}px`,
+          }}
+        >
+          {label && (
+            <label className={labelClass} ref={labelRef}>
+              {label}
+            </label>
+          )}
+        </div>
         <div className={controlRightClass} />
       </Flex>
       <div className={style["select-control-dropdown-indicator"]}>
         {open ? (
           <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M6 15l6-6 6 6" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M6 15l6-6 6 6" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
           </svg>
         ) : (
           <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M6 9l6 6 6-6" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M6 9l6 6 6-6" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
           </svg>
         )}
       </div>
