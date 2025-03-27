905a4588ffa9e0ccb99d2436de04400fa8266c1e
HyeBinYang <skdisk7368@gmail.com>
2025-03-27 15:18:34 +0900

    feat: Button Props color 더 범용성있게 수정

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -3,6 +3,7 @@ import Button from ".";
 import Flex from "../Flex";
 import type { Meta, StoryObj } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
+import color from "../../constant/color";
 
 const meta: Meta<typeof Button> = {
   title: "Components/Button",
@@ -15,9 +16,14 @@ const meta: Meta<typeof Button> = {
     ),
   ],
   argTypes: {
+    variant: {
+      control: "select",
+      options: ["contained", "outlined", "ghost"],
+      description: "버튼 모양",
+    },
     color: {
       control: "select",
-      options: ["primary", "secondary", "tertiary"],
+      options: Object.keys(color),
       description: "버튼 색상",
     },
     size: {
@@ -47,55 +53,57 @@ type Story = StoryObj<typeof meta>;
 
 export const Test: Story = {
   args: {
-    color: "primary",
+    variant: "contained",
+    color: "Blue400",
     size: "large",
+    disabled: false,
     children: "Test",
     onClick: action("Test Button Clicked"),
   },
 };
 
-export const Primary = () => {
+export const Contained = () => {
   return (
     <>
-      <Button color="primary" size="large" onClick={action("Primary Large Button Clicked")}>
+      <Button variant="contained" color="Blue400" size="large" onClick={action("Contained Large Button Clicked")}>
         Large
       </Button>
-      <Button color="primary" size="medium" onClick={action("Primary Medium Button Clicked")}>
+      <Button variant="contained" color="Blue400" size="medium" onClick={action("Contained Medium Button Clicked")}>
         Medium
       </Button>
-      <Button color="primary" size="small" onClick={action("Primary Small Button Clicked")}>
+      <Button variant="contained" color="Blue400" size="small" onClick={action("Contained Small Button Clicked")}>
         Small
       </Button>
     </>
   );
 };
 
-export const Secondary = () => {
+export const Outlined = () => {
   return (
     <>
-      <Button color="secondary" size="large" onClick={action("Secondary Large Button Clicked")}>
+      <Button variant="outlined" color="Blue400" size="large" onClick={action("Outlined Large Button Clicked")}>
         Large
       </Button>
-      <Button color="secondary" size="medium" onClick={action("Secondary Medium Button Clicked")}>
+      <Button variant="outlined" color="Blue400" size="medium" onClick={action("Outlined Medium Button Clicked")}>
         Medium
       </Button>
-      <Button color="secondary" size="small" onClick={action("Secondary Small Button Clicked")}>
+      <Button variant="outlined" color="Blue400" size="small" onClick={action("Outlined Small Button Clicked")}>
         Small
       </Button>
     </>
   );
 };
 
-export const Tertiary = () => {
+export const Ghost = () => {
   return (
     <>
-      <Button color="tertiary" size="large" onClick={action("Tertiary Large Button Clicked")}>
+      <Button variant="ghost" color="Blue400" size="large" onClick={action("Ghost Large Button Clicked")}>
         Large
       </Button>
-      <Button color="tertiary" size="medium" onClick={action("Tertiary Medium Button Clicked")}>
+      <Button variant="ghost" color="Blue400" size="medium" onClick={action("Ghost Medium Button Clicked")}>
         Medium
       </Button>
-      <Button color="tertiary" size="small" onClick={action("Tertiary Small Button Clicked")}>
+      <Button variant="ghost" color="Blue400" size="small" onClick={action("Ghost Small Button Clicked")}>
         Small
       </Button>
     </>
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,20 +1,23 @@
 import React from "react";
 import style from "./Button.module.css";
 import classNames from "classnames";
+import colors from "../../constant/color";
 
-type ButtonColor = "primary" | "secondary" | "tertiary";
+type ButtonVariant = "contained" | "outlined" | "ghost";
+type ButtonColor = keyof typeof colors;
 type ButtonSize = "large" | "medium" | "small";
 type Props = {
+  variant: ButtonVariant;
   color: ButtonColor;
   size: ButtonSize;
 } & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
-const Button = ({ children, color, size, ...props }: React.PropsWithChildren<Props>) => {
+const Button = ({ children, variant, color, size, ...props }: React.PropsWithChildren<Props>) => {
   const buttonClass = classNames(
     {
-      [style.primary]: color === "primary",
-      [style.secondary]: color === "secondary",
-      [style.tertiary]: color === "tertiary",
+      [style.contained]: variant === "contained",
+      [style.outlined]: variant === "outlined",
+      [style.ghost]: variant === "ghost",
       [style.large]: size === "large",
       [style.medium]: size === "medium",
       [style.small]: size === "small",
@@ -23,7 +26,16 @@ const Button = ({ children, color, size, ...props }: React.PropsWithChildren<Pro
   );
 
   return (
-    <button className={buttonClass} {...props}>
+    <button
+      className={buttonClass}
+      {...props}
+      style={{
+        backgroundColor: variant === "contained" ? colors[color] : undefined,
+        borderColor: variant === "outlined" ? colors[color] : undefined,
+        color: variant === "contained" ? undefined : colors[color],
+        ...props.style,
+      }}
+    >
       {children}
       <span className={style["button-background"]} />
     </button>
