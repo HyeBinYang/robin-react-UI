e4a7efc0b22477b33db0bf2d9fe98eaaaabd0c74
HyeBinYang <skdisk7368@gmail.com>
2025-03-27 19:08:02 +0900

    feat: Select 컴포넌트 빌드 input에 추가

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ import "./global.module.css";
 export { default as Button } from "./components/Button";
 export { default as Checkbox } from "./components/Checkbox";
 export { default as Flex } from "./components/Flex";
+export { default as Select } from "./components/Select";
