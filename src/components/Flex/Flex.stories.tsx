2a088f8610a13543b50c05c04ec97a62eebf892a
HyeBinYang <skdisk7368@gmail.com>
2025-03-30 23:40:39 +0900

    docs: Flex 스토리 추가

---
+++ src/components/Flex/Flex.stories.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import type { Meta, StoryObj } from "@storybook/react";
+import Flex from ".";
+import Button from "../Button";
+
+const meta: Meta<typeof Flex> = {
+  title: "Layouts/Flex",
+  component: Flex,
+  argTypes: {},
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Flex>;
+
+export const Example: Story = {
+  args: {
+    children: (
+      <>
+        <Button>Button 1</Button>
+        <Button>Button 2</Button>
+        <Button>Button 3</Button>
+        <Button>Button 4</Button>
+        <Button>Button 5</Button>
+        <Button>Button 6</Button>
+        <Button>Button 7</Button>
+        <Button>Button 8</Button>
+        <Button>Button 9</Button>
+        <Button>Button 10</Button>
+      </>
+    ),
+  },
+};
